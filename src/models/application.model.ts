6b8079b16463b4448d2c63ae602ea19ecab8738c
Ngo Quang Thang <ngoquangthang2003@gmail.com>
2024-06-06 23:59:14 +0700

    add models

---
+++ src/models/application.model.ts
@@ -0,0 +1,63 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Application extends Entity {
+  @property({
+    type: 'number',
+    id: true,
+    generated: true,
+  })
+  id?: number;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  fullName: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  email: string;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  phone: number;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  resume: string;
+
+  @property({
+    type: 'string',
+  })
+  coverLetter?: string;
+
+  @property({
+    type: 'date',
+    defaultFn: 'now',
+  })
+  appliedAt?: Date;
+
+  @property({
+    type: 'string',
+    default: 'pending',
+  })
+  status?: 'pending' | 'accepted' | 'rejected';
+
+
+  constructor(data?: Partial<Application>) {
+    super(data);
+  }
+}
+
+export interface ApplicationRelations {
+  // describe navigational properties here
+}
+
+export type ApplicationWithRelations = Application & ApplicationRelations;
